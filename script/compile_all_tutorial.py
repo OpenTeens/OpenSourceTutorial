2a9b97b2d3ff80be29dfc6bdb3bf8230b8229080
berniehuang2008 <berniehuang2008@163.com>
2024-10-01 13:35:35 +0800

    auto do

--- script/compile_all_tutorial.py
+++ script/compile_all_tutorial.py
@@ -3,17 +3,18 @@ import hashlib
 import json
 
 all_tutorials = []
-for folder in os.listdir("tutorial/src"):
-    all_tutorials.extend(
-        list(map(lambda x: f"{folder}/{x}", os.listdir(f"tutorial/src/{folder}")))
-    )
+for root, dirs, files in os.walk("tutorial/src"):
+    for file in files:
+        if file.endswith(".md"):
+            relative_path = os.path.relpath(f"{root}/{file}", "tutorial/src").replace("\\", "/")
+            all_tutorials.append(relative_path)
 
 tutorial_map = {}
 for tutorial in all_tutorials:
     compiled_name = hashlib.md5(tutorial.encode()).hexdigest() + ".html"
     tutorial_map[tutorial] = compiled_name
     os.system(
-        f"python lib/tutorial-maker/src/main.py -i tutorial/src/{tutorial} -o tutorial/build/{compiled_name} -a lib/tutorial-maker/src"
+        f"python lib/tutorial-maker/src/main.py -i \"tutorial/src/{tutorial}\" -o \"tutorial/build/{compiled_name}\" -a lib/tutorial-maker/src"
     )
 
 json.dump(tutorial_map, open("tutorial/build/map.json", "w"))
---
+++ script/render_all.py
@@ -0,0 +1,66 @@
+import json
+import jinja2
+
+
+map_json = json.load(open("tutorial/build/map.json"))
+
+for name in map_json:
+    html_path = f"tutorial/build/{map_json[name]}"
+
+    # parse map_json into tree-like dict
+    def parse_map_json(map_json):
+        tree = {}
+        for key, value in map_json.items():
+            key = key.split("/")
+            t = tree
+            for k in key[:-1]:
+                if k not in t:
+                    t[k] = {}
+                t = t[k]
+            t[key[-1]] = value
+
+        return tree
+
+
+    tree = parse_map_json(map_json)
+
+    # cut tree
+    name = name.split("/")
+    if len(name) > 2:
+        for i in range(0, len(name) - 2):
+            tree = tree[name[i]]
+
+    nav_tree_html = ""
+
+
+    def render_nav_tree(tree, depth=0):
+        global nav_tree_html
+        for key, value in tree.items():
+            class_name = "collapsed"
+            if key == name[depth + 1]:
+                class_name = "active"
+
+            if type(value) == str:
+                nav_tree_html += f"<li class='{class_name} url' onclick=\"goto_url('{value}')\">{key}</li>"
+            else:
+                nav_tree_html += (
+                    f"<li class='{class_name}' onclick='expand_navt(this)'>{key}<ul>"
+                )
+                render_nav_tree(value, depth + 1)
+                nav_tree_html += "</ul></li>"
+
+
+    # print(tree)
+    render_nav_tree(tree)
+
+    # render html
+    with open(html_path) as f:
+        article_html = f.read()
+
+    template = jinja2.Template(open("script/template/single_article.html").read())
+    html = template.render(
+        article=article_html, nav_tree=nav_tree_html, title=name[-1].split(".")[0]
+    )
+
+    with open(html_path, "w") as f:
+        f.write(html)
